3bf644c03b53255b541de645dd93bd59021853ad
AgentGregory <grege2005@gmail.com>
2022-05-05 23:54:39 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Matrix<Integer> arr = new Matrix<>({1,2,3}{4,5,6});
+    }
+}
---
+++ src/Matrix.java
@@ -0,0 +1,32 @@
+public class Matrix<Integer extends Number> {
+
+    public Matrix(Integer[][] arr) {
+
+    }
+
+    public void plusMatrix(int arr[][], int arr1[][]) {
+        int[][] arr2 = new int[arr.length][arr1[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr2[i][j] = arr[i][j] + arr1[i][j];
+            }
+
+        }
+
+    }
+
+    public void multiMatrix(int arr[][], int arr1[][]) {
+        int[][] arr2 = new int[arr.length][arr1[0].length];
+
+        for (int i = 0; i < arr.length; i++) { //умножаю массив на массив
+            for (int j = 0; j < arr1[0].length; j++) {
+                for (int k = 0; k < arr1.length; k++) {
+                    arr2[i][j] = arr2[i][j] + arr[i][k] * arr1[k][j];
+                }
+
+            }
+
+        }
+    }
+
+}
